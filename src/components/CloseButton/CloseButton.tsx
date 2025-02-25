63cbb01cf4eda3ae929eb8b6893d165eafeab8b7
ChoiYoungKyu <mpd253@naver.com>
2025-02-25 11:01:31 +0900

    🚨Fix: conflict 오류 수정

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -6,11 +6,7 @@ export default function CloseButton() {
   return (
     <Link
       href="/lobby"
-<<<<<<< Updated upstream:src/components/CloseButton/CloseButton.tsx
       className="bg-[var(--color-ligthRed)] hover:bg-[var(--color-ligthRed-hover)] lg:p-4 md:p-2 lg:rounded-[12px] md:rounded-[8px]"
-=======
-      className="bg-[var(--color-lightRed)] lg:p-4 md:p-2 lg:rounded-[12px] md:rounded-[8px]"
->>>>>>> Stashed changes:src/components/Button/CloseButton.tsx
     >
       <Image
         src={close}
