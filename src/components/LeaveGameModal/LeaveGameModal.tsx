b15a91963faf7879b2e534a48cf5576d8f9e820a
Ajihey77 <anjihey77@gmail.com>
2025-02-28 11:07:54 +0900

    🚨 Fix : 모달수정

--- src/components/LeaveGameModal/LeaveGameModal.tsx
+++ src/components/LeaveGameModal/LeaveGameModal.tsx
@@ -14,14 +14,19 @@ export default function LeaveGameModal({
   return (
     <Modal
       title={"나가기"}
-      width={"w-[788px]"}
+      width={"xl:w-[788px] md:w-[60%] w-[80%]"}
       height={"h-[268px]"}
       setIsClose={setIsClose}
       setIsComplete={() => router.push("/lobby")}
     >
-      <div className="flex items-center justify-center text-2xl bg-[var(--color-point)] w-[707px] h-[96px] rounded-xl">
-        <Image src={info} alt="경고 아이콘" className="2xl:h-32" />
-        <div className="text-white">게임에서 나가시겠습니까 ?</div>
+      <div
+        className="
+          flex items-center justify-center bg-[var(--color-point)] 
+          xl:text-xl text-md 
+          xl:w-[707px] w-[80%] h-[96px] rounded-xl"
+      >
+        <Image src={info} alt="경고 아이콘" className="xl:h-32 h-16" />
+        <div className="text-white">게임에서 나가시겠습니까?</div>
       </div>
     </Modal>
   );
