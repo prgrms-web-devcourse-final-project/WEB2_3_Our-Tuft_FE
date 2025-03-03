acb67abb7c82ee85f67a5cae21efb537c65738ef
ChoiYoungKyu <mpd253@naver.com>
2025-03-03 22:50:53 +0900

    🔨Refactor: defaultFetch 에러 처리 로직 추가

--- src/service/api/defaultFetch.ts
+++ src/service/api/defaultFetch.ts
@@ -1,29 +1,45 @@
-type fetchOptions = {
-  method?: "GET" | "POST" | "PUT" | "DELETE";
-  headers?: Record<string, string>;
-  body?: string;
+type FetchOptions = {
+  method?: "GET" | "POST" | "PUT" | "DELETE"; // HTTP 메서드
+  headers?: Record<string, string>; // 요청 헤더
+  body?: string; // 요청 본문 (주로 POST, PUT에서 사용)
 };
 
 export async function defaultFetch<T>(
-  endpoint: string,
-  options: fetchOptions = {}
+  endpoint: string, // (ex: "/users", "posts/1" etc..)
+  options: FetchOptions = {}
 ): Promise<T> {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}${endpoint}`,
-    {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        // 여기에 토큰 넣기
-        // Authorization: `Bearer `,
-        ...(options.headers || {}),
-      },
-    }
-  );
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+  if (!API_BASE_URL) {
+    throw new Error("API_BASE_URL is not defined");
+  }
+
+  // 세션 스토리지에서 토큰 가져오기
+  // const token = typeof window !== "undefined" ? sessionStorage.getItem("token") : null;
+
+  const res = await fetch(`${API_BASE_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      // 여기에 토큰 넣기
+      // ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...(options.headers || {}),
+    },
+  });
+
+  let data: unknown; // JSON 파싱 실패 대비
+
+  // 응답 데이터 처리
+  try {
+    data = await res.json();
+  } catch (error) {
+    throw new Error(`Catch API Error: ${res.status}`);
+  }
+
+  // 응답 상태 체크 (API 응답 실패, ex) HTTP 400 error)
   if (!res.ok) {
     throw new Error(`API Error: ${res.status}`);
   }
 
-  return res.json();
+  return data as T;
 }
