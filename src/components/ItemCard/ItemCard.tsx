a4f192e6956da93899ee49bb7577d05eb187ad58
ChoiYoungKyu <mpd253@naver.com>
2025-03-06 02:49:13 +0900

    ✨Feature: 포인트 상점 데이터 렌더링(수정 예정)

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 interface ItemCardProps {
-  imgSrc: string;
+  imageUrl: string;
   name: string;
 }
 
-export default function ItemCard({ imgSrc, name }: ItemCardProps) {
+export default function ItemCard({ imageUrl, name }: ItemCardProps) {
   return (
     <div className="grid grid-rows-3 grid-cols-2 bg-[var(--color-second)] w-full h-18 h-36 md:h-24 lg:h-28 xl:h-36 2xl:h-56 px-5 py-3 max-[480px]:px-3 md:px-3.5 md:py-2 xl:px-5 xl:py-3 max-[480px]:gap-x-2.5 gap-x-5 gap-y-2.5 md:gap-x-2.5 md:gap-y-1.5 lg:gap-x-5 lg:gap-y-2.5 rounded-2xl">
       <div className="row-span-3 bg-[var(--color-main)] rounded-2xl flex items-center justify-center">
-        <span className="text-center">{imgSrc}</span>
+        <span className="text-center">{imageUrl}</span>
       </div>
       <div className="row-span-1 bg-[var(--color-point)] rounded-2xl flex items-center justify-center">
         <span className="text-white text-center md:text-base lg:text-lg xl:text-xl 2xl:text-3xl">
