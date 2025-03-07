795f114dabc0f133e7807386f233d695331d5c7f
Ajihey77 <anjihey77@gmail.com>
2025-03-07 20:01:27 +0900

    임시 OX 구현 (수정예정)

--- src/containers/game/oxQuize/OXMain/index.tsx
+++ src/containers/game/oxQuize/OXMain/index.tsx
@@ -1,8 +1,59 @@
 import OXList from "./oxList";
 import ChatBubble from "../../../../components/ChatBubble";
 import UserCard from "../../../../components/UserCard";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
-export default function OXMain() {
+const socket = io("http://localhost:8080");
+
+export default function OXMain({
+  chat,
+  oxAnswer,
+}: {
+  chat: { id: string; chat: string; chatId: string }[];
+  oxAnswer: boolean | null;
+}) {
+  const [user, setUser] = useState<{ id: string; answer?: boolean | null }[]>(
+    []
+  );
+  // const [answer, setAnswer] = useState<{ user: string; answer: boolean }[]>([]);
+  const [userox, setUserox] = useState<{ id: string; answer?: boolean }[]>([]);
+  type GroupedData = { [key: string]: (typeof chat)[0][] };
+  const groupedById = chat.reduce((acc: GroupedData, item) => {
+    if (!acc[item.id]) {
+      acc[item.id] = [];
+    }
+    acc[item.id].push(item);
+    return acc;
+  }, {});
+
+  useEffect(() => {
+    // chat 배열을 통해 uniqueIds 생성 (id 기준으로 중복 제거)
+    const uniqueIds = Array.from(
+      new Map(chat.map((chat) => [chat.id, chat])).values()
+    );
+
+    // 기존에 있는 값들은 그대로 두고 answer만 null로 초기화
+    const initializedUser = uniqueIds.map((chat) => ({
+      id: chat.id,
+      answer: null,
+    }));
+
+    setUser(initializedUser);
+  }, [chat]);
+
+  useEffect(() => {
+    socket.on("answer", (msg: { id: string; answer: boolean }) => {
+      setUser((prev) => {
+        const updatedUser = prev.map((user) =>
+          user.id === msg.id ? { ...user, answer: msg.answer } : user
+        );
+
+        return updatedUser;
+      });
+    });
+  }, []);
+
   return (
     <div
       className="
@@ -14,21 +65,34 @@ export default function OXMain() {
         xl:overflow-visible
       "
     >
-      {[0, 1, 2, 3, 4].map((i, index) => (
+      {user.map((i, index) => (
         <div key={index}>
           <div className="flex flex-col">
             <div className="hidden 2xl:flex flex-col justify-end h-[200px]">
-              {[0, 1, 2].map((i, index) => (
-                <ChatBubble key={index} />
-              ))}
+              {Object.entries(groupedById)
+                .filter(([id]) => id === i.id)
+                .flatMap(([_, messages]) =>
+                  messages.map((msg, index) => (
+                    <ChatBubble msg={msg} key={msg.chatId} />
+                  ))
+                )}
             </div>
             <UserCard
               bgColor={"bg-[#ffd377]"}
               imageSize={"h-40"}
               textSize2={"2xl:text-[18px] md:text-[28px] sm:text-[20px]"}
               padding={"p-3"}
+              nickname={i.id}
+              key={index}
+              oxAnswer={oxAnswer === i.answer ? true : false}
             >
-              <OXList />
+              <OXList answer={i.answer!} key={index} />
+
+              {/* {oxAnswer
+                .filter((g) => g.user === i)
+                .map((item, index) => (
+                  <OXList answer={item.answer} key={index} />
+                ))} */}
               <div
                 className="absolute 2xl:static justify-center 2xl:pt-2 md:text-3xl sm:text-xl"
                 style={{
--- src/containers/game/oxQuize/OXMain/oxList/index.tsx
+++ src/containers/game/oxQuize/OXMain/oxList/index.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import OImg from "@/assets/images/O-img.png";
-// import XImg from "@/assets/images/X-img.png";
+import XImg from "@/assets/images/X-img.svg";
 
-export default function OXList() {
+export default function OXList({ answer }: { answer?: boolean }) {
+  if (answer === undefined || answer === null) return null;
+
   return (
-    <div className="absolute 2xl:-top-31 right-1 md:right-2 2xl:right-7">
-      <Image src={OImg} alt="이미지" className="flex 2xl:w-32 w-14" />
+    <div className="absolute 2xl:-top-33 right-1 md:right-2 2xl:right-7">
+      {answer ? (
+        <Image src={OImg} alt="O 이미지" className="flex 2xl:w-32 w-14" />
+      ) : (
+        <Image src={XImg} alt="X 이미지" className="flex 2xl:w-32 w-14" />
+      )}
     </div>
   );
 }
