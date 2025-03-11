1c88ff6f1f875a8f4effafebc72485c638c14d4c
ChoiYoungKyu <mpd253@naver.com>
2025-03-11 16:43:34 +0900

    ✨Feat: Next.js 빌드 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["team09-bucket.s3.ap-northeast-2.amazonaws.com"],
   },
+  compiler: {
+    removeConsole: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
